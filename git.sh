041cc95a7c4966b2c504fc1b1cd28725e0d603ec
mogagnon <mogagnon1@gmail.com>
2024-10-20 10:33:19 -0400

    rework auth a bit

--- git.sh
+++ git.sh
@@ -12,22 +12,6 @@ Host github.com
   IdentityFile $SSH_FILE_PATH
 "
 
-if ! gh auth status > /dev/null 2>&1; then
-  echo "Logging into GitHub using gh"
-  gh auth login -h github.com -p ssh -w
-else
-  echo "Already authenticated with GitHub."
-fi
-
-scopes=$(curl -s -I -H "Authorization: token $(gh auth token)" https://api.github.com/user | grep 'X-OAuth-Scopes:')
-
-if echo "$scopes" | grep -q "write:gpg_key"; then
-  echo "Token has the write:gpg_key permission."
-else
-  echo "Token does NOT have the write:gpg_key permission."
-  gh auth refresh -s write:gpg_key
-fi
-
 echo "Setup git configuration"
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_EMAIL"
@@ -51,7 +35,21 @@ fi
 echo "$SSH_CONFIG" >> "$HOME/.ssh/config"
 ssh-add --apple-use-keychain "$SSH_FILE_PATH"
 
-gh auth login -h github.com -p ssh -w
+if ! gh auth status > /dev/null 2>&1; then
+  echo "Logging into GitHub using gh"
+  gh auth login -h github.com -p ssh -w
+else
+  echo "Already authenticated with GitHub."
+fi
+
+scopes=$(curl -s -I -H "Authorization: token $(gh auth token)" https://api.github.com/user | grep 'X-OAuth-Scopes:')
+
+if echo "$scopes" | grep -q "write:gpg_key"; then
+  echo "Token has the write:gpg_key permission."
+else
+  echo "Token does NOT have the write:gpg_key permission."
+  gh auth refresh -s write:gpg_key
+fi
 
 echo "Setup GPG key for commit signature"
 echo "Do you want to set a passphrase for the GPG key? (y/n)"
